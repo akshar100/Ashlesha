89228e2b613876b7c961e2e463b53d3e1c94ee2a
Akshar Prabhu Desai <akshar@akshar.co.in>
2012-04-12 10:34:36 +0000

    Moved to YUI3.5.0

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -16,7 +16,7 @@
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-    <script src="http://yui.yahooapis.com/3.5.0pr4/build/yui/yui-min.js"></script>
+    <script src="http://yui.yahooapis.com/3.5.0/build/yui/yui-min.js"></script>
   </head>
 
 <body>
