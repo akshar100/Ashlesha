8081a70a93fb50c8865726bbdd24e8224d115efd
Akshar Prabhu Desai <akshar@akshar.co.in>
2012-05-24 22:17:21 +0000

    Language file paramterized

---
+++ application/language/english/default_lang_default.php
@@ -0,0 +1,28 @@
+<?PHP
+$lang["site_title"] = "Brand A Brand Custom Instance";
+$lang["site_name"] = "{SITE_NAME}";
+$lang["site_description"] = "BrandABrand Custom Instance.";
+$lang["site_copyright"] = "&copy; ".date("Y",time())." ".$lang["site_name"]; //add year authomatically
+$lang["sign_up_pitch"] = "It's quick";
+$lang["main_post_title"] = "Post";
+$lang["tags"] = "Topics e.g. Howto,rules";
+$lang["categorization"] = "Category";
+$lang["post"] = "Post";
+$lang["group"] = "Project Group";
+$lang["quiz"] = "Quiz"; 
+$lang['quiz_subject'] = "Request to answer the Quiz";
+$lang['hero_unit_title'] = 'BrandABrand Custom Instance';
+$lang['hero_unit_text'] = $lang["site_description"];
+
+$lang['about_title'] = 'About '.$lang["site_name"];
+
+$lang['about_text'] = <<<DATA
+<p>
+No About Us Information Provided.  					
+</p>
+DATA;
+$lang['tos_title'] = 'Terms and Conditions (TOS)';
+$lang['tos_text'] = <<<DATA
+<p>TOS are not yet defined. You can define them from the admin panel</p>
+DATA;
+
